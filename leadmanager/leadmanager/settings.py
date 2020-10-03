0c8423b12a29a90a64f7a63b0014aa078a71d79f
Alvis <chewjta@gmail.com>
2020-10-04 06:27:55 +0800

    Update settings.py

--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')m&^-xh50-*dfhb9zif@fymc$ifqx(x(plov^v$__iw^5e3z6v'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
